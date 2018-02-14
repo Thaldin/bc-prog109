cf31f9dec9c3158de3800415509c1e33044c5e94
Thaldin <thaldin@comcast.net>
2018-02-13 20:08:22 -0800

    Module 5

---
+++ prog109/prog109/module5/js/module5.js
@@ -0,0 +1,32 @@
+﻿// First call for page load
+doWork();
+
+function doWork() {
+
+    var input = document.getElementById('number'); // Unit of table
+    var num = Number(input.value);
+
+    if (num > 10 || num < 0) {
+        document.getElementById("error").style.display = "block";
+    }
+    else {
+        document.getElementById("error").style.display = "none";
+        multiply(num);
+    }
+}
+
+function multiply(table) {
+
+    var i = 1;  // Counter
+    var msg = '<h2>Multiplication Table</h2>'; // Message
+
+    // Do multiplication
+    while (i < 11) {
+        msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+        i++;
+    }
+
+    // Write the message into the page
+    var el = document.getElementById('answer');
+    el.innerHTML = msg;
+}
