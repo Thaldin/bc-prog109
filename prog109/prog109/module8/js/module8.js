a3c7f5f8f271eb8a55cfd55e9f861f4f7524bbce
Thaldin <thaldin@comcast.net>
2018-03-02 21:06:23 -0800

    Module8

---
+++ prog109/prog109/module8/js/module8.js
@@ -0,0 +1,18 @@
+﻿function AddItem() {
+
+    if (grocery === "") {
+        return;
+    }
+
+    var grocery = document.getElementById("grocery").value
+
+    var newEl = document.createElement("li");
+    var newItem = document.createTextNode(grocery);
+    newEl.appendChild(newItem);
+
+    var list = document.getElementById("todo");
+    var count = list.childNodes.length;
+    newEl.setAttribute("id", count + 1);
+    
+    list.appendChild(newEl);
+}
