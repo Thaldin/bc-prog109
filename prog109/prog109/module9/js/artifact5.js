0a277a8b137bb57c6b2ca4fa0b82f258a58576e9
Thaldin <thaldin@comcast.net>
2018-03-10 12:19:22 -0800

    Carousel

--- prog109/prog109/module9/js/artifact5.js
+++ prog109/prog109/module9/js/artifact5.js
@@ -1,1 +1,54 @@
-﻿
+﻿var nextButton = document.getElementById("next");
+var prevButton = document.getElementById("prev");
+var autoCheck = document.getElementById("auto");
+var carouselList = document.getElementById("carouselList");
+var timerFunction;
+var maxCount = carouselList.getElementsByTagName("li").length;
+var currentItem = 0;
+
+getCarouselEntry(currentItem);
+
+// Enable events
+autoCheck.addEventListener("click", enableAutoCycle, false);
+
+nextButton.addEventListener("click", function () {
+    cycleCarousel(1);
+});
+
+prevButton.addEventListener("click", function () {
+    cycleCarousel(-1);
+});
+
+// Event functions
+function cycleCarousel(change) {
+
+    currentItem = currentItem + change;
+    if (currentItem >= maxCount) {
+        currentItem = 0;
+    }
+    else if (currentItem < 0) {
+        currentItem = maxCount;
+    }
+
+    getCarouselEntry(currentItem);
+}
+
+function enableAutoCycle() {
+    if (this.checked === false) {
+        clearInterval(timerFunction);
+    }
+    else {
+        timerFunction = setInterval(function () {
+            cycleCarousel(1);
+        }, 2000);
+    }
+}
+
+function getCarouselEntry(index) {
+
+    var imgSrc = carouselList.getElementsByTagName("li")[index].attributes["data-img"].value;
+    var imgDesc = carouselList.getElementsByTagName("li")[index].innerHTML;
+
+    document.getElementById("img-container").setAttribute("src", imgSrc);
+    document.getElementById("img-desc").innerHTML = imgDesc;
+}
