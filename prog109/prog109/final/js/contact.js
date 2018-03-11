2128ea468e7a34d2b91fbe36a69cfda88a77439d
Thaldin <thaldin@comcast.net>
2018-03-10 18:18:33 -0800

    modules section

---
+++ prog109/prog109/final/js/contact.js
@@ -0,0 +1,30 @@
+﻿function validateContact() {
+
+    var success = true;
+    var errMsg = "";
+
+    console.log("HI");
+
+    var name = document.getElementById("contact-name");
+    var email = document.getElementById("contact-email");
+    var msg = document.getElementById("contact-msg");
+
+    if (name.value.length === 0) {
+        success = false;
+        errMsg += "Must provide a name. <br />";
+    }
+
+    if (email.value.length === 0) {
+        success = false;
+        errMsg += "Must provide an email address. <br />";
+    }
+
+    if (msg.value.length === 0) {
+        success = false;
+        errMsg += "Must enter a message. <br />";
+    }
+
+    document.getElementById("contact-err").innerHTML = errMsg;
+
+    return success;
+}
---
+++ prog109/prog109/final/js/modules.js
@@ -0,0 +1,20 @@
+﻿
+var modules = document.getElementsByClassName("module");
+
+for (var i = 0; i < modules.length; i++) {
+
+    modules[i].addEventListener("mouseover", enterModule, false);
+    modules[i].addEventListener("mouseout", exitModule, false);
+}
+
+function enterModule() {
+    var moduleNum = this.attributes["data-id"].value;
+    var moduleDesc = document.getElementById(moduleNum).innerText;
+    this.innerText = moduleDesc;
+}
+
+function exitModule() {
+    var moduleNum = this.attributes["data-id"].value;
+    var moduleDesc = document.getElementById(moduleNum).attributes["data-module"].value;
+    this.innerText = moduleDesc;
+}
